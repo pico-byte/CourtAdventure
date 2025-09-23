6b427d1eeb053dd2fe20cf9f2fa6577e36abfaeb
Naturelist <andreastree@icloud.com>
2025-09-23 14:30:34 +0200

    full JSON data compatibility added; dialogue class expansion

--- src/Dialogue.java
+++ src/Dialogue.java
@@ -1,6 +1,19 @@
+import com.google.gson.internal.bind.util.ISO8601Utils;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
 
 public class Dialogue {
     String text;
-    boolean needsAnswer;
+    ArrayList<String> options;
+    Dialogue nextDialogue;
+
+
+    public Dialogue(String text, String... options){
+        this.text = text;
+        this.options = new ArrayList<>(Arrays.asList(options));
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
 import com.google.gson.*;
+import com.google.gson.internal.bind.util.ISO8601Utils;
 
 import java.io.IOException;
 import java.nio.file.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String json = Files.readString(Path.of("src/package.json"));
-        JsonObject obj = JsonParser.parseString(json).getAsJsonObject();
+        String json = new String(Files.readAllBytes(Paths.get("/Users/Naturelist/Desktop/Dev/Code/School/LK/TextAdventure/TextAdventure/src/data.json")));
+        JsonObject data = JsonParser.parseString(json).getAsJsonObject();
 
+        JsonArray dialogueArray = data.getAsJsonArray("Dialogue");
+
+        for (int i = 0; i < dialogueArray.size(); i++) {
+            JsonObject d = dialogueArray.get(i).getAsJsonObject();
+
+            String text = d.get("text").getAsString();
+
+            JsonArray options = d.getAsJsonArray("options");
+            for (int j = 0; j < options.size(); j++) {
+                System.out.println("  option: " + options.get(j).getAsString());
+            }
+        }
+
+
+
+
+
+
+        // parse the root object but not into a class
+        JsonObject obj = JsonParser.parseString(json).getAsJsonObject();
 
         String name = obj.get("name").getAsString();
         int age = obj.get("age").getAsInt();
