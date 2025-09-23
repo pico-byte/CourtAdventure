afbad18d3cd72f5426e11dbdb9010f7212deb662
Naturelist <andreastree@icloud.com>
2025-09-23 11:01:55 +0200

    JSON compatibility added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
-import java.util.Scanner;
+import com.google.gson.*;
+
+import java.io.IOException;
+import java.nio.file.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String json = Files.readString(Path.of("src/package.json"));
+        JsonObject obj = JsonParser.parseString(json).getAsJsonObject();
 
-        IO.out("Wilkommen zu Court Adventure!!! 🧑‍⚖️ ");
+
+        String name = obj.get("name").getAsString();
+        int age = obj.get("age").getAsInt();
+        IO.out("Name: " + name + ", Age: " + age);
+
+        IO.out("GO TO HELL");
     }
 };
